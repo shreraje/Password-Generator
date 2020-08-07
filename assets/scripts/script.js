2b526f503e0f1081a7cf838c351c65bcfd6eea80
shreraje <rajendrashr@gmail.com>
2020-08-07 11:37:58 -0700

    Adding variables for random password generation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variables criteria
+var number = ('0123456789');
+var special = ('!@#$%^&*');
+var uppercase = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+var lowercase = ('abcdefghijklmnopqrstuvwxyz');
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
