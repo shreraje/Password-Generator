434da25be17d9f127e4c28a28318a0dd8c20e065
shreraje <rajendrashr@gmail.com>
2020-08-08 20:50:52 -0700

    Final script file added with all the changed needed to be made

--- script.js
+++ script.js
@@ -18,3 +18,48 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// function for user to choose a number to generate a random password
+function generatePassword() {
+  var limit = prompt("Please choose a number of characters for your random new password. It must be between 8 and 128 number.");
+  var choice = "";
+  if (limit < 8 || limit > 128) {
+    alert("You must choose a number between 8 and 128");
+ 
+    return generatePassword();
+  }
+
+  // Variables to confirm if user uses any of the follwing characters:
+var num = confirm("Any number you want to use for your new password?");
+var specialchar = confirm("Any special character you want to use for your new password?");
+var upper = confirm("Any uppercase letter you want to use for your new password?");
+var lower = confirm("Any lowercase letter you want to use for your new password?");
+
+
+// If and if else loop
+if (num && specialchar && upper && lowercase) {
+  choice = (choice + number);
+  choice = (choice + special);
+  choice += uppercase;
+  choice += lowercase;
+
+
+  // If user wish not to choose uppercase, lowercase, numbers and special characters,
+  // OR user wish not  to answer any of the questions, 
+  // user will get alert and returns to option to generate password again.
+} else if (choice === "") {
+  alert ("You must choose at least one type from Uppercase, Lowercase, Numbers and special characters.")
+  return generatePassword();
+}
+
+
+var newPassword = "";
+
+for (i= 0 ; i <= limit; i++) {
+
+  newPassword += choice.charAt(Math.floor(Math.random() * (choice.length)));
+
+}
+return newPassword;
+}
