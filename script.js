2e57a9c022d5b1ca955c9678d9d6245a03521de2
shreraje <rajendrashr@gmail.com>
2020-08-08 18:55:10 -0700

    Adding js and css file in main page of password generator repository

---
+++ script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Variables criteria
+var number = ("0123456789");
+var special = ("!@#$%^&*()");
+var uppercase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+var lowercase = ("abcdefghijklmnopqrstuvwxyz");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+  
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
